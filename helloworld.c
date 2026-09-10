f60ae540ef00fdbbb8822a98e61776fafe0f3378
Kanariebrood <kw.dejong@student.avans.nl>
2026-09-10 13:47:53 +0200

    Ny first feature commit

--- helloworld.c
+++ helloworld.c
@@ -5,4 +5,6 @@ printf("Hello, Galaxy!"); //person 1
 printf("Hello, Universe!"); // person 2
 printf("Hello, Solarsystem!"); // person 3
 printf("Hello, avans"); // person 3
+
+printf("NIEUWE FEATURE"); // nieuwe feature
 }
