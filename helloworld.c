2fa108f679b1d76fb730048c939f3cf08365d959
Kanariebrood <kw.dejong@student.avans.nl>
2026-09-10 13:18:17 +0200

    My second commit

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
-   printf("Hello, World!");
+   printf("Hello, Galaxy!"); //person 1
+	printf("Hello, Universe!"); // person 2
+	printf("Hello, Solarsystem!"); // person 3
+	printf("asdfghj"; // persoon 3+
+
    return 0;
 }
